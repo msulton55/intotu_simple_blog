afe88f4c5c4cf3a45cb6fcefce67fb4bcd3d92e6
msulton55 <msulton55@gmail.com>
2020-07-24 16:40:13 +0700

    adding update function

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -5,19 +5,24 @@
 				<h3>Title</h3>
 			</div>
 			<div class="row">
-				<input type="text" readonly class="form-control" id="title" v-bind:value="post.title">
+				<input type="text" class="form-control" id="title" v-bind:value="post.title">
 			</div>
 			<div class="row">
 				<h3>Body Content</h3>
 			</div>
 			<div class="row">
-				<input type="text" readonly class="form-control" id="body" v-bind:value="post.body">
+				<input type="text" class="form-control" id="body" v-bind:value="post.body">
 			</div>
+			<br />
+			<div class="row">
+        <button class="btn btn-primary" name="action" v-on:click="updatePost">Update</button>
+      </div>
 		</div>
 	</div>
 </template>
 
 <script>
+	import axios from 'axios'
 	export default {
 		name: "Detail",
 		data() {
@@ -33,6 +38,22 @@
 					.then((result) => {
 						this.post = result.data
 					})
+			},
+			
+			updatePost: async function () {
+				let title = document.getElementById("title").value
+        let body = document.getElementById("body").value
+
+        axios.post('https://dummy-api.winfad.com/api/posts/' + this.post.id + '/update', {
+            title: title,
+            body: body
+          })
+          .then(function (response) {
+            alert("Success Update!")
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
 			}
 		},
 		mounted: function () {
